c77ea9bf683a67e0cad0b404f0c0a5d5f612e0cf
Alexander Roidl <alexander.roidl@hs-mainz.de>
2024-02-15 16:08:01 +0100

    add text comp

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import RossmannSynagoge from "./components/RossmannSynagoge.vue"
+import TextAudioPlayer from "./components/TextAudioPlayer.vue"
 </script>
 
 <template>
   <RossmannSynagoge/>
+  <TextAudioPlayer/>
 </template>
 
 <style scoped>
---
+++ src/components/TextAudioPlayer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+  
+    </div>  
+  </template>
+  
+  <script>
+  export default {
+    name: "TextAudioPlayer",
+    data() {
+      return {
+        jsonData: null
+      }
+    },
+    async created() {
+      const response = await fetch('/transcript.json');
+      this.jsonData = await response.json();
+      
+    }
+  }
+  </script>  
+  
+  <style scoped>
+    .container {
+      position: relative;
+    }
+  </style>
