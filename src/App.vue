09dc325426781d2674d8f3ca160ec15b47eef8fc
Alexander Roidl <alexander.roidl@hs-mainz.de>
2024-02-15 15:55:37 +0100

    base

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import RossmannSynagoge from "./components/RossmannSynagoge.vue"
+</script>
+
+<template>
+  <RossmannSynagoge/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RossmannSynagoge.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <img src="../assets/2_rueckgeb.jpeg" alt="test">
+    <img src="../assets/3_rueckost.jpeg" alt="test">
+  </div>  
+</template>
+
+<script>
+export default {
+  name: "RossmannSynagoge"
+}
+</script>
+
+<style scoped>
+  .container {
+    position: relative;
+  }
+   img {
+     width: 400px;
+   }
+</style>
