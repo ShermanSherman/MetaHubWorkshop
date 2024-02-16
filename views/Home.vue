61b9f0928660541172a16ff7c75b3890899fb955
Alexander Roidl <alexander.roidl@hs-mainz.de>
2024-02-16 16:51:11 +0100

    main page

--- views/Home.vue
+++ views/Home.vue
@@ -2,16 +2,17 @@
 import RossmannSynagoge from "../src/components/RossmannSynagoge.vue"
 </script>
 <template>
-  <h1>Comment here:</h1>
+  <!-- <h1>Comment here:</h1>
     <router-link to="/textsource/1">Text Source 1: Capitol</router-link><br>
     <router-link to="/textsource/2">Text Source 2: Friedhof</router-link><br>
     <router-link to="/textsource/3">Text Source 3: Rossmann</router-link><br>
-  <br>
-  <h1>Read / show here:</h1>
-    <router-link to="/text/1">Text  1: Capitol</router-link><br>
-    <router-link to="/text/2">Text  2: Friedhof</router-link><br>
-    <router-link to="/text/3">Text  3: Rossmann</router-link><br>
-    <RossmannSynagoge />
+  <br> -->
+  <h1>Mapping Offenbach / 11.Februar – 16.Februar 2024 </h1>
+    <router-link to="/text/3">Rossmann</router-link><br>
+    <router-link to="/text/1">Capitol</router-link><br>
+    <router-link to="/text/2">Friedhof</router-link><br>
+    
+  
 </template>
 
 <script>
@@ -22,4 +23,20 @@ export default {
 
 <style scoped>
 
+h1{
+  font-family: "castledownDotted";
+  letter-spacing: 0.016em;
+  padding-left: 0.2em;
+}
+a{
+  font-size: 5em;
+  color: black;
+  text-decoration: none;
+}
+
+a:hover{
+  font-size: 5em;
+  color: red;
+  text-decoration: none;
+}
 </style>
